bc5d2670fbd38e277d1319ed6c99c6a14f4a210f
humblezxc <stopenchuk.yaroslav@gmail.com>
2025-09-03 21:00:00 +0200

    Created Nest and MongoDB connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ExpensesModule } from './expenses/expenses.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            autoSchemaFile: true,
+        }),
+        MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/expenses'),
+        CategoriesModule,
+        ExpensesModule,
+    ],
 })
 export class AppModule {}
