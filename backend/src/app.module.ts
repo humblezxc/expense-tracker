79e9d9ccf34b63104d0836cc2b23b50e0be48503
humblezxc <stopenchuk.yaroslav@gmail.com>
2025-09-03 20:51:00 +0200

    Added backend scaffold

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,19 +1,10 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ExpensesModule } from './expenses/expenses.module';
-import { CategoriesModule } from './categories/categories.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-    imports: [
-        GraphQLModule.forRoot<ApolloDriverConfig>({
-            driver: ApolloDriver,
-            autoSchemaFile: true,
-        }),
-        MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/expenses'),
-        CategoriesModule,
-        ExpensesModule,
-    ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
