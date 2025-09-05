5557f8d292db4fc79f3a38beb01c08b65a0bd68d
humblezxc <stopenchuk.yaroslav@gmail.com>
2025-09-05 17:55:13 +0200

    Updated theme, minor styles update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ApolloProvider } from '@apollo/client/react';
-import { Provider } from 'react-redux';
 import { client } from "./lib/apolloClient.ts";
 
 import App from './App';
@@ -10,7 +9,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ApolloProvider client={client}>
-                <App />
+            <App />
         </ApolloProvider>
     </React.StrictMode>,
 );
--- frontend/src/pages/Categories.tsx
+++ frontend/src/pages/Categories.tsx
@@ -1,6 +1,6 @@
 import {GET_CATEGORIES} from "../queries/categories.ts";
 import {useQuery} from "@apollo/client/react";
-import type {Category} from "../../types/graphql.ts";
+import type {Category} from "../types/graphql.ts";
 
 interface GetCategoriesData {
     categories: Category[];
@@ -13,7 +13,7 @@ export default function Categories() {
     if (error) return <p>Error: {error.message}</p>;
 
     return (
-        <div className="p-4">
+        <div className="py-4">
             <h1 className="text-xl font-bold mb-2">Categories</h1>
             <ul className="list-disc ml-5">
                 {data?.categories.map(category => (
